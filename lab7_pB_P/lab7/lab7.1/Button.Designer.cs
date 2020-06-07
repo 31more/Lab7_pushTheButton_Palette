88da56e48d63f39c9fbe0ccb4231791588df79f1
Kozhevnikova Polina <58529592+31more@users.noreply.github.com>
2020-06-08 00:14:26 +0300

    Add files via upload

---
+++ lab7_pB_P/lab7/lab7.1/Button.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace lab7._1
+{
+    partial class Running_Button
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button.Location = new System.Drawing.Point(206, 139);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(180, 50);
+            this.button.TabIndex = 0;
+            this.button.Text = "Try to push me ;)";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Running_Button
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.button);
+            this.Name = "Running_Button";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Push Me";
+            this.Load += new System.EventHandler(this.Running_Button_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+    }
+}
+
